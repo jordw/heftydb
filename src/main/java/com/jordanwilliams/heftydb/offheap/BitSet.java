4b3a0a061ac8a3df066305e8efe16e6c968e5251
Jordan Williams <jordanwilliams33@gmail.com>
2013-12-21 16:27:45 -0800

    Add offheap bitset + simple test

---
+++ src/main/java/com/jordanwilliams/heftydb/offheap/BitSet.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2013. Jordan Williams
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jordanwilliams.heftydb.offheap;
+
+import com.jordanwilliams.heftydb.util.Sizes;
+
+public class BitSet {
+
+    private final static int ADDRESS_BITS_PER_WORD = 6;
+    private final Memory memory;
+
+    public BitSet(long numberOfBits){
+        memory = Memory.allocate((memoryOffset(numberOfBits - 1) + 1) * Sizes.LONG_SIZE);
+    }
+
+    public void set(long index, boolean value) {
+        int offset = memoryOffset(index);
+
+        if (value){
+            //Set
+            memory.setLong(offset, memory.getLong(offset) | (1L << index));
+        } else {
+            //Clear
+            memory.setLong(offset, memory.getLong(offset) & ~(1L << index));
+        }
+    }
+
+    public boolean get(long index){
+        int offset = memoryOffset(index);
+        return ((memory.getLong(offset) & (1L << index)) != 0);
+    }
+
+    private static int memoryOffset(long bitIndex) {
+        return (int) ((bitIndex >> ADDRESS_BITS_PER_WORD) * Sizes.LONG_SIZE);
+    }
+}
---
+++ src/test/java/com/jordanwilliams/heftydb/test/unit/offheap/BitSetTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2013. Jordan Williams
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jordanwilliams.heftydb.test.unit.offheap;
+
+import com.jordanwilliams.heftydb.offheap.BitSet;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+
+public class BitSetTest {
+
+    @Test
+    public void getSetTest(){
+        BitSet testSet = new BitSet(256);
+        boolean[] values = new boolean[256];
+
+        Random random = new Random(System.nanoTime());
+
+        for (int i = 0; i < 256; i++){
+            boolean nextValue = random.nextBoolean();
+            testSet.set(i, nextValue);
+            values[i] = nextValue;
+        }
+
+        for (int i = 0; i < 256; i++){
+            Assert.assertEquals("Values match", values[i], testSet.get(i));
+        }
+    }
+}
